eeb0bb8f2a6c61342f9133ad45ae74b38be35f19
sree23456 <sreenivasboddaboyina@gmail.com>
2022-02-01 16:55:21 +0530

    sreenivasyadav

---
+++ src/org/samp/Students.java
@@ -0,0 +1,17 @@
+package org.samp;
+
+public class Students {
+	public static int result;
+
+	public static void main(String[] args) {
+	int number=567;
+	 result =567%0;
+	while (number>0) {
+		int num=number%10;
+		result=(result*10)+number;
+		num=num/10;
+	}
+	System.out.println("result");	
+	}
+
+}
